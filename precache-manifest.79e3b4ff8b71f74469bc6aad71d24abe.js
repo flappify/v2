8950432bedd3e6bb36610a755a7995c3637e37d3
Flappify <creative.flappify@gmail.com>
2019-12-17 21:24:08 +0530

    file upload

---
+++ precache-manifest.79e3b4ff8b71f74469bc6aad71d24abe.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c9077912537f57df5fa48f922c73a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffb53b6245b6456f84b6",
+    "url": "/static/css/2.d3ea48d6.chunk.css"
+  },
+  {
+    "revision": "ac8e3fc66e62abefee2c",
+    "url": "/static/css/main.a09f3279.chunk.css"
+  },
+  {
+    "revision": "ffb53b6245b6456f84b6",
+    "url": "/static/js/2.8a61d3fa.chunk.js"
+  },
+  {
+    "revision": "ac8e3fc66e62abefee2c",
+    "url": "/static/js/main.aa144346.chunk.js"
+  },
+  {
+    "revision": "4d725ddf0292724cad14",
+    "url": "/static/js/runtime-main.cc34514a.js"
+  },
+  {
+    "revision": "a41a6c816f406b74ac88bfd58bc46b33",
+    "url": "/static/media/404.a41a6c81.jpg"
+  },
+  {
+    "revision": "d112f7834b847d59cb1c9b4bc45b1c01",
+    "url": "/static/media/about.d112f783.png"
+  },
+  {
+    "revision": "9df944096cb27e21bfa0420cb59d4e36",
+    "url": "/static/media/analysis.9df94409.png"
+  },
+  {
+    "revision": "84aaec3ced6fc4bd87e88cfa9b1fbad9",
+    "url": "/static/media/branding.84aaec3c.png"
+  },
+  {
+    "revision": "db343c71bef4e3b17339de43e905cbb3",
+    "url": "/static/media/construction.db343c71.jpg"
+  },
+  {
+    "revision": "ef29088a10fc0f3274a37ed6ce2b0a35",
+    "url": "/static/media/contactform.ef29088a.png"
+  },
+  {
+    "revision": "22cad29c5caa297106e176cfe90c6712",
+    "url": "/static/media/development.22cad29c.png"
+  },
+  {
+    "revision": "5c477fe7e8176aba42137855c7b9301e",
+    "url": "/static/media/digitalmarketing.5c477fe7.png"
+  },
+  {
+    "revision": "f39a614118f5c5f8f1a2d7532b73eac4",
+    "url": "/static/media/intropart1.f39a6141.png"
+  },
+  {
+    "revision": "c5877706a7da668c610ad5cf2886f561",
+    "url": "/static/media/intropart2.c5877706.png"
+  },
+  {
+    "revision": "a7999582effeb4bb142952cce5a23df7",
+    "url": "/static/media/intropart3.a7999582.png"
+  },
+  {
+    "revision": "e2cb1388a1bfefed99ffeed7031e8e17",
+    "url": "/static/media/language1.e2cb1388.png"
+  },
+  {
+    "revision": "ed8e3f4ec819bcbf545e582989a51500",
+    "url": "/static/media/language2.ed8e3f4e.png"
+  },
+  {
+    "revision": "0cbe2cd78c60990bb95568ba8a125976",
+    "url": "/static/media/language3.0cbe2cd7.png"
+  },
+  {
+    "revision": "dd98ef08439a1c4a7cc9e96814493b5d",
+    "url": "/static/media/language4.dd98ef08.png"
+  },
+  {
+    "revision": "668f7180dbbddcc9d9dc9dfd8967db4f",
+    "url": "/static/media/language5.668f7180.png"
+  },
+  {
+    "revision": "8371d8d0731523dbd2f702d8f8abff4e",
+    "url": "/static/media/language6.8371d8d0.png"
+  },
+  {
+    "revision": "63c6450d6cced66b5e2d3781276dfdc0",
+    "url": "/static/media/login.63c6450d.jpg"
+  },
+  {
+    "revision": "e5f1e6b89701bb6ec757371ad99645d3",
+    "url": "/static/media/pattern.e5f1e6b8.png"
+  },
+  {
+    "revision": "2b48a8dcbd1f482d265fcdb769525d54",
+    "url": "/static/media/productdesign.2b48a8dc.png"
+  },
+  {
+    "revision": "347c9b0f7bc423735b6ae5ac8ca348f4",
+    "url": "/static/media/seoanalysis.347c9b0f.png"
+  },
+  {
+    "revision": "c4a28ec2f257dd2476117c57fd7afbd1",
+    "url": "/static/media/userexperience.c4a28ec2.png"
+  },
+  {
+    "revision": "3eeedabb223edb3ceb6aea063f2c94e8",
+    "url": "/static/media/whychooseus.3eeedabb.png"
+  },
+  {
+    "revision": "83873d147e96bf55864576eedfda5484",
+    "url": "/static/media/workprocess2.83873d14.png"
+  },
+  {
+    "revision": "29a6e2985cf0c27d6e3a4870142e23e3",
+    "url": "/static/media/workprocess3.29a6e298.png"
+  },
+  {
+    "revision": "6b7f09a851f32e006f974395681e60a4",
+    "url": "/static/media/workprocess4.6b7f09a8.png"
+  }
+]);
